c35e88ced6372abad6541559024f2541d3619ab9
Trisha <swayam.prajapati.962004@gmail.com>
2025-08-27 12:00:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Mediscope-AI",
+    version="0.1.0",
+    author="Swayam Prajapati",
+    author_email="swayam.prajapati.962004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
